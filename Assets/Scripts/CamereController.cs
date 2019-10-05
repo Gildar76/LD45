24a081ea334972e646d76be9b1afb66a8b918271
Fredrik Andersson <gildar76@gmail.com>
2019-10-05 02:00:02 +0200

    Implement basic movement

---
+++ Assets/Scripts/CamereController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GildarGaming.LD45
+{
+    public class CamereController : MonoBehaviour
+    {
+        public Transform cameraTarget;
+        public float damping;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        void LateUpdate()
+        {
+            Vector3 cameraPos = transform.position;
+            cameraPos = Vector3.Lerp(cameraPos, cameraTarget.transform.position, Time.deltaTime * 60f);
+            cameraPos.z = transform.position.z;
+            transform.position = cameraPos;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GildarGaming.LD45
+{
+    public class PlayerController : MonoBehaviour
+    {
+        Rigidbody2D rb;
+        [SerializeField] float speed;
+        
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+
+        }
+
+
+        void FixedUpdate()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            Vector2 movementVector = new Vector2();
+            movementVector.x = Input.GetAxis("Horizontal");
+            movementVector.y = Input.GetAxis("Vertical");
+            rb.AddForce(movementVector * speed);
+        }
+    }
+
+}
