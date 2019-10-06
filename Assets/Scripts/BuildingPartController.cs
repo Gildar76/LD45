d98631343c516939fa01cde3ca94f9c084f231ec
Fredrik Andersson <gildar76@gmail.com>
2019-10-06 17:31:42 +0200

    Implement rocket building

---
+++ Assets/Scripts/BuildingPartController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GildarGaming.LD45
+{
+    public enum BuildingPart
+    {
+        Main,Upper,LeftEngine,RightEngine,Holder
+    }
+    public class BuildingPartController : MonoBehaviour
+    {
+        public BuildingPart part;
+
+    }
+
+}
---
+++ Assets/Scripts/RocletController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GildarGaming.LD45
+{
+    public class RocletController : MonoBehaviour
+    {
+
+        public GameObject mainBody;
+        public GameObject leftEngine;
+        public GameObject rightEngine;
+        public GameObject upper;
+        public GameObject holder;
+        bool allPartsActive = false;
+
+        void Start()
+        {
+            holder.SetActive(false);
+            mainBody.SetActive(false);
+            upper.SetActive(false);
+            leftEngine.SetActive(false);
+            rightEngine.SetActive(false);
+            
+        }
+        void AddPart(BuildingPart part)
+        {
+            Debug.Log("Adding part" + part);
+            switch (part)
+            {
+                case BuildingPart.Main:
+                    mainBody.SetActive(true);
+                    break;
+                case BuildingPart.Upper:
+                    upper.SetActive(true);
+                    break;
+                case BuildingPart.LeftEngine:
+                    leftEngine.SetActive(true);
+                    break;
+                case BuildingPart.RightEngine:
+                    rightEngine.SetActive(true);
+                    break;
+                case BuildingPart.Holder:
+                    holder.SetActive(true);
+                    break;
+                
+            }
+            CheckActiveParts();
+        }
+
+        private void CheckActiveParts()
+        {
+            allPartsActive = true;
+            if (!holder.activeInHierarchy) allPartsActive = false;
+            if (!mainBody.activeInHierarchy) allPartsActive = false;
+            if (!upper.activeInHierarchy) allPartsActive = false;
+            if (!leftEngine.activeInHierarchy) allPartsActive = false;
+            if (!rightEngine.activeInHierarchy) allPartsActive = false;
+
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            //Not the best way to check tags, but will do for now.
+            if (allPartsActive && other.gameObject.tag == "Player")
+            {
+                AttackProbe(other.gameObject);
+                InitializeFlightSequence();
+            }
+            BuildingPartController bpc = other.gameObject.GetComponent<BuildingPartController>();
+            if (bpc != null)
+            {
+                AddPart(bpc.part);
+                Destroy(other.gameObject);
+            }
+        }
+
+        private void AttackProbe(GameObject player)
+        {
+            PlayerController pc = player.GetComponent<PlayerController>();
+            pc.enabled = false;
+            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+            player.transform.position = holder.transform.position;
+
+        }
+
+        private void InitializeFlightSequence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
