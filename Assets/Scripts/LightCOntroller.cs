460824e3597a532321a108c39d13c0bb7506779d
Fredrik Andersson <gildar76@gmail.com>
2019-10-06 05:54:51 +0200

    More uncommitted day 1 stuff

---
+++ Assets/Scripts/LightCOntroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCOntroller : MonoBehaviour
+{
+    [SerializeField] Light dirLight;
+    [SerializeField] Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.y < 0)
+        {
+            dirLight.intensity = 1 + (player.position.y/100f);
+        }
+    }
+}
