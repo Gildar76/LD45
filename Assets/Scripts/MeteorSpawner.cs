d9b173be8ebba98e7ed34f8c5321a0acf6065b5a
Fredrik Andersson <gildar76@gmail.com>
2019-10-06 02:48:05 +0200

    Commit for the entire first day...

---
+++ Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GildarGaming.LD45
+{
+    public class MeteorSpawner : MonoBehaviour
+    {
+
+        [SerializeField] GameObject meteorPrefab;
+        float spawnTimer = 0f;
+        [SerializeField] float spawnDelay = 3f;
+        [SerializeField] Vector3 spawnLocation;
+        [SerializeField] float minSpawnX;
+        [SerializeField] float maxSpawnX;
+        [SerializeField] float spawnY;
+        [SerializeField] Vector3 spawnVelocity;
+        void Start()
+        {
+            
+        }
+
+
+        void Spawn()
+        {
+            SetVelocityAndLocation();
+            Instantiate(meteorPrefab, spawnLocation, Quaternion.identity);
+            spawnTimer = 0;
+        }
+
+        void SetVelocityAndLocation()
+        {
+            spawnVelocity = new Vector3(Random.Range(-50f, 50f), Random.Range(0f, -1f), 0f);
+            spawnLocation = new Vector3(Random.Range(minSpawnX, maxSpawnX), spawnY, 0f);
+
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer >= spawnDelay)
+            {
+                Spawn();
+            }
+        }
+    }
+
+}
